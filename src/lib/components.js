fdf3aacc0bdd2e8f4231b2de2d055d664a68b32a
mickle0629 <mikeyd2k2@gmail.com>
2024-07-11 22:30:25 -0700

    fixed floating commas with null region names

--- src/lib/components.js
+++ src/lib/components.js
@@ -15,8 +15,8 @@ export function DayForecast( { locationData, forecastDaysByHour }) {
   console.log("Day forecast data:", forecastDaysByHour);
   return (
     <section className="temperature-forecast">
-      <h1 className="temperature-forecast__title">Today's 24-hr Forecast</h1>
-      <h2 className="temperature-forecast__location-name">{locationData.name}, {locationData.region}</h2>
+      <h1 className="temperature-forecast__title">Today's 24-hr Forecast</h1>  {/* VVVVV some places dont have a region name, so this makes sure there's no floating commas */}
+      <h2 className="temperature-forecast__location-name">{locationData.name} {locationData.region && ","}{locationData.region}</h2>
       <div className="temperature-forecast__cards">
         <Card top="Now" mid={forecastDaysByHour[0].temp_c} isForecastCard={true}/>
         {forecastDaysByHour.slice(1).map((hour) => (
